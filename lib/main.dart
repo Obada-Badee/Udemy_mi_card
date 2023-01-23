57b1c4404ae45a3a8d60606ed1747e0d939fb885
Obada A.Albadee <obadabadee3@hotmail.com>
2023-01-23 23:22:43 +0200

    assets are added and incorprated into the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,8 +17,32 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(color: Colors.amber, child: Text("Hello world")),
-        ),
+            child: Column(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/obada.jpg'),
+            ),
+            Text(
+              'Obada Badee',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico'),
+            ),
+            Text(
+              'CONTROL ENGINEER',
+              style: TextStyle(
+                  color: Colors.teal[100],
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Source San Pro',
+                  letterSpacing: 2.5),
+            )
+          ],
+        )),
       ),
     );
   }
