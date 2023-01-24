a75afed54e6fb07868417eb6cc3e04d37825b701
Obada A.Albadee <obadabadee3@hotmail.com>
2023-01-25 00:20:10 +0200

    finished project

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           // ignore: prefer_const_literals_to_create_immutables
           children: [
             CircleAvatar(
@@ -35,12 +36,62 @@ class MyApp extends StatelessWidget {
             Text(
               'CONTROL ENGINEER',
               style: TextStyle(
-                  color: Colors.teal[100],
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Source San Pro',
-                  letterSpacing: 2.5),
-            )
+                color: Colors.teal[100],
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Source San Pro',
+                letterSpacing: 2.5,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal[100],
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal[900],
+                ),
+                title: Text(
+                  "+249 121 789 218 ",
+                  style: TextStyle(
+                    fontFamily: 'Source San Pro',
+                    color: Colors.teal[900],
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.mail,
+                  color: Colors.teal[900],
+                ),
+                title: Text(
+                  "obadabadee3@hotmail.com",
+                  style: TextStyle(
+                    fontFamily: 'Source San Pro',
+                    color: Colors.teal[900],
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
           ],
         )),
       ),
